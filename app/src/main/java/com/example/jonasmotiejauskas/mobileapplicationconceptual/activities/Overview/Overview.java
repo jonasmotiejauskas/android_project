30d54c0210fdc62ca0346297e14ed7e020636271
dbtst <jonasmotiejauskas@gmail.com>
2018-09-22 23:58:51 +0300

    initial commit

---
+++ app/src/main/java/com/example/jonasmotiejauskas/mobileapplicationconceptual/activities/Overview/Overview.java
@@ -0,0 +1,15 @@
+package com.example.jonasmotiejauskas.mobileapplicationconceptual.activities.Overview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jonasmotiejauskas.mobileapplicationconceptual.R;
+
+public class Overview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overview);
+    }
+}
